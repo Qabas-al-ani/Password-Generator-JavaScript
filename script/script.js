14fd8747e9015368189eb3690e20d820485fa095
Qabas al ani <doctorqa@yahoo.com>
2021-06-17 19:57:50 -0400

    updated

--- script/script.js
+++ script/script.js
@@ -14,8 +14,8 @@ generateBtn.addEventListener("click", writePassword);
 // here is the generate Password function that creates the password
 function generatePassword() {
   // declaring the variables that is used
-  const upperCaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const lowerCaseletters = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
   const numbers = "0123456789";
   const symbols = "!@#$%^&*_-+=";
 
@@ -26,7 +26,7 @@ function generatePassword() {
   let includeSymbols;
 
   // we used the prompt to enter the length
-  const length = parseInt(
+  let length = parseInt(
     prompt(
       "Please enter the length of the password and it should be from 8 to 128 characters!."
     )
@@ -58,14 +58,14 @@ function generatePassword() {
 
   // check what characters to add to the characters string
   let characters = "";
-  includeUpperCaseLetters ? (characters += upperCaseletters) : "";
-  includeLowerCaseLetters ? (characters += lowerCaseletters) : "";
+  includeUpperCaseLetters ? (characters += upperCaseLetters) : "";
+  includeLowerCaseLetters ? (characters += lowerCaseLetters) : "";
   includeNumbers ? (characters += numbers) : "";
   includeSymbols ? (characters += symbols) : "";
 
   // generate and return a random password frim the characters string
   let password = "";
-  for (i = 0; i < length; i++) {
+  for (let i = 0; i < length; i++) {
     password += characters.charAt(
       Math.floor(Math.random() * characters.length)
     );
